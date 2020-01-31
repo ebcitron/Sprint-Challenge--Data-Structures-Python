db29d97dcf4e64c4b3481e3dd548504571293d28
Eli Citron <ebcitron@gmail.com>
2020-01-31 13:08:06 -0600

    Debugging, had it the first time

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,33 +8,49 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        #If self.storage.length is at capacity:
+        #If storage is empty, add item as head and set as current.
+        if len(self.storage) == 0:
+            self.storage.add_to_head(item)
+            self.current = self.storage.head
+        #If storage is not at capacity, add to tail and set tail as current
+        elif len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
+        #If the current node is the last node, you reconsider the head node as oldest, remove it and add the new item in its place, set it to current
+        elif self.current == self.storage.tail:
+            self.storage.remove_from_head()
+            self.storage.add_to_head(item)
+            self.current = self.storage.head
+        #If self.storage.length is at capacity, insert item after current,  adjust storage length, set new item as current. 
         if self.storage.length == self.capacity:
-            #Keep track of where the most current node is, which will be the tail
-            #If its empty, you may set its value  == to item
-            if self.current is None:
-                self.current = self.storage.tail
-            self.current.value = item
-            #If it has any previous nodes, set the previous one up as current
-            if self.current.prev:
-                self.current = self.current.prev
-            else:
-                self.current = self.storage.tail
+            self.current.insert_after(item)
+            self.storage.length += 1
+            self.current = self.current.next
+            self.storage.delete(self.current.next)
 
-    
-        else:
-             self.storage.add_to_head(item)
-        
+
+
+            #if self.storage.length == self.capacity:
+            # #Keep track of where the most current node is, which will be the tail
+            # #If its empty, you may set its value  == to item
+            # if self.current is None:
+            #     self.current = self.storage.tail
+            # self.current.value = item
+            # #If it has any previous nodes, set the previous one up as current
+            # if self.current.prev:
+            #     self.current = self.current.prev
+            # else:
+            #     self.current = self.storage.tail
 
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-        current = self.storage.tail
+        current = self.storage.head
         while current:
             if current.value is not None:
                 list_buffer_contents.append(current.value)
-            current = current.prev
+            current = current.next
         # TODO: Your code here
 
         return list_buffer_contents
