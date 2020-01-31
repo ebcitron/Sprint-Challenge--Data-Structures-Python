744d894ff231672eaeaf0f84861c2b8e53f8e677
Eli Citron <ebcitron@gmail.com>
2020-01-31 12:31:08 -0600

    Getting started, using last time as example

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -9,18 +9,29 @@ class RingBuffer:
 
     def append(self, item):
         #If self.storage.length is at capacity:
-            #if self.currentitem = none: set it as the tail of self.storage
-                 #self.current.value = item.value
-            #if self.current.previous:
+        if self.storage.length == self.capacity:
+            if self.current is None:
+                self.current = self.storage.tail
+            self.current.value = item
+            if self.current.prev:
+                self.current = self.current.prev
+            else:
+                self.current = self.storage.tail
 
-        #else self.storage.add_to_head(item)
+    
+        else:
+             self.storage.add_to_head(item)
         
 
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
- 
+        current = self.storage.tail
+        while current:
+            if current.value is not None:
+                list_buffer_contents.append(current.value)
+            current = current.prev
         # TODO: Your code here
 
         return list_buffer_contents
